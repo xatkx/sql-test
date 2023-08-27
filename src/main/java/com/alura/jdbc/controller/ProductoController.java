9a51ee76e3ba54e24364a90949b46eea67a4788d
xatkx <xatkx1@gmail.com>
2023-08-27 11:24:16 -0400

    utilizando el try-with-resources

--- src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -15,104 +15,104 @@ import java.util.Map;
 public class ProductoController {
 
     public void modificar(String nombre, String descripcion, Integer id) throws SQLException {
-        
+
         String query = "UPDATE producto SET nombre = ? ,descripcion = ? WHERE id = ? ";
-        
-        Connection connect = new connectFactory().create();
-        
-        PreparedStatement s = connect.prepareStatement(query);
-        
-        s.setString(1, nombre);
-        s.setString(2, descripcion);
-        s.setInt(3,id);
-        
-        boolean b = s.execute();
-        
-        System.out.println(descripcion);
-        connect.close();
-        
+
+        final Connection connect = new connectFactory().create();
+
+        try (connect) {
+            PreparedStatement s = connect.prepareStatement(query);
+            s.setString(1, nombre);
+            s.setString(2, descripcion);
+            s.setInt(3, id);
+            boolean b = s.execute();
+        }
     }
 
     public void eliminar(Integer id) throws SQLException {
         String query = "delete from producto where id = ?";
-        
-        Connection connect = new connectFactory().create();
-        PreparedStatement statement = connect.prepareStatement(query);
-        statement.setInt(1,id);
-        statement.execute();
+
+        final Connection connect = new connectFactory().create();
+
+        try (connect) {
+            PreparedStatement statement = connect.prepareStatement(query);
+            statement.setInt(1, id);
+            statement.execute();
 //        System.out.println(query);
-        connect.close();
+        }
     }
 
-    public List<Map<String,String>> listar() throws SQLException {
+    public List<Map<String, String>> listar() throws SQLException {
 
-        List<Map<String,String>> productos = new ArrayList<>();
-     
-        Connection connect = new connectFactory().create();
+        List<Map<String, String>> productos = new ArrayList<>();
 
-        PreparedStatement statement = connect.prepareStatement("select id, nombre, descripcion, cantidad from producto");
+        final Connection connect = new connectFactory().create();
 
-        boolean isRead = statement.execute();
-        if (isRead) {
-            
-            ResultSet result = statement.getResultSet();
+        try (connect) {
+            final PreparedStatement statement = connect.prepareStatement("select id, nombre, descripcion, cantidad from producto");
 
-            while (result.next()) {
-                Map<String,String> producto = new HashMap<String,String>();
-                
-                producto.put("id", result.getString(1));
-                producto.put("nombre", result.getString(2));
-                producto.put("descripcion", result.getString(3));
-                producto.put("cantidad", result.getString(4));
-                
+            try (statement) {
+                boolean isRead = statement.execute();
+                if (isRead) {
+
+                    ResultSet result = statement.getResultSet();
+
+                    while (result.next()) {
+                        Map<String, String> producto = new HashMap<String, String>();
+
+                        producto.put("id", result.getString(1));
+                        producto.put("nombre", result.getString(2));
+                        producto.put("descripcion", result.getString(3));
+                        producto.put("cantidad", result.getString(4));
+
 //                result.
-                productos.add(producto);
-                
-//                System.out.println(result.getString(1)+result.getString(1));
-            }
-            
-//            System.out.println("klk");
-        }
-        connect.close();
+                        productos.add(producto);
 
-        return productos;
+//                System.out.println(result.getString(1)+result.getString(1));
+                    }
+                }
+
+//            System.out.println("klk");
+            }
+            return productos;
+        }
     }
 
     public void guardar(Map<String, String> producto) throws SQLException {
         int maxCantidad = 50;
         int cantidad = Integer.valueOf(producto.get("cantidad"));
         String query = "insert into producto ( nombre, descripcion, cantidad) values (?,?,?)";
-        
-        Connection connect = new connectFactory().create();
-        connect.setAutoCommit(false);
-        PreparedStatement statement = connect.prepareStatement(query);
-        
-        try {
-            while(cantidad > 0)
-        {
-           
-            int resto = Math.min(cantidad, maxCantidad);
-            guardarDatos(producto,resto,statement);
-            cantidad -= maxCantidad;
+
+        final Connection connect = new connectFactory().create();
+        try (connect) {
+            connect.setAutoCommit(false);
+            final PreparedStatement statement = connect.prepareStatement(query);
+
+            try (statement) {
+                while (cantidad > 0) {
+
+                    int resto = Math.min(cantidad, maxCantidad);
+                    guardarDatos(producto, resto, statement);
+                    cantidad -= maxCantidad;
+                }
+                System.out.println("success");
+            } catch (Exception e) {
+
+                connect.rollback();
+                System.out.println("rollback");
+            }
+            connect.commit();
+
         }
-            System.out.println("success");
-        } catch (Exception e) {
-            
-            connect.rollback();
-            System.out.println("rollback");
-        }
-        connect.commit();
-        connect.close();
-        
+
     }
-    
-    public void guardarDatos(Map<String,String> producto,int resto, PreparedStatement statement) throws SQLException
-    {
+
+    public void guardarDatos(Map<String, String> producto, int resto, PreparedStatement statement) throws SQLException {
 //        if(resto < 50)
 //        {
 //            throw new RuntimeException("esto es un error mmg");
 //        }
-        
+
         statement.setString(1, producto.get("nombre"));
         statement.setString(2, producto.get("descripcion"));
         statement.setInt(3, resto);
